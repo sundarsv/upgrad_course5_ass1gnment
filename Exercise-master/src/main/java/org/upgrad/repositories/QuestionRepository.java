a6952f835a6db0cdda65edb4894e3f3aa5d5433f
Apoorva2405 <apurva.sharma2405@gmail.com>
2018-07-12 22:21:36 +0530

    changes for generated id issue

--- Exercise-master/src/main/java/org/upgrad/repositories/QuestionRepository.java
+++ Exercise-master/src/main/java/org/upgrad/repositories/QuestionRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import org.upgrad.models.Question;
 
 import javax.transaction.Transactional;
+import java.sql.ResultSet;
 
 /*
     Author - Apoorva
@@ -19,13 +20,16 @@ public interface QuestionRepository extends CrudRepository<Question,Integer> {
 
     @Transactional
     @Modifying
-    @Query(nativeQuery = true,value="insert into question (id,content,date,user_id) values (DEFAULT,?2,NOW(),?3)")
+    @Query(nativeQuery = true,value="insert into question (id,content,date,user_id) values (DEFAULT,?1,NOW(),?2)")
     void addQuestion(String content, int user_id);
+
+    @Query(nativeQuery = true,value="SELECT currval(pg_get_serial_sequence('question', 'id'))")
+    int getInsertedId();
 
     @Transactional
     @Modifying
     @Query(nativeQuery = true,value="insert into question_category (id,question_id,category_id) values (DEFAULT,?2,?3)")
-    void addCategory(int id, int question_id, int category_id);
+    void addCategory(int question_id, int category_id);
 
     @Query(nativeQuery = true,value="select * from question where id IN ?1")
     Iterable<Question> getQuestionsByQuestionId(Iterable<Integer> id);
--- Exercise-master/src/main/java/org/upgrad/services/QuestionServiceImp.java
+++ Exercise-master/src/main/java/org/upgrad/services/QuestionServiceImp.java
@@ -1,10 +1,12 @@
 package org.upgrad.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 import org.upgrad.models.Question;
 import org.upgrad.repositories.QuestionRepository;
 
+import java.sql.ResultSet;
 import java.util.Set;
 
 /*
@@ -22,10 +24,10 @@ public class QuestionServiceImp implements QuestionService {
     @Override
     public void addQuestion(String content, int user_id,Set<Integer> categoryId) {
         questionRepository.addQuestion(content,user_id);
-//        for(Integer catId : categoryId) {
-//            Long id = System.currentTimeMillis() % 1000;
-//            questionRepository.addCategory(id.intValue(), question_id, catId);
-//        }
+        int question_id = questionRepository.getInsertedId();
+        for(Integer catId : categoryId) {
+            questionRepository.addCategory(question_id, catId);
+        }
     }
 
     @Override
