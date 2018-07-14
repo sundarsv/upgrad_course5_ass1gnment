d37547d9b5740b356ee43b082ea7241963c6878d
Apoorva2405 <apurva.sharma2405@gmail.com>
2018-07-14 13:52:31 +0530

    fixing admin controller tests

--- Exercise-master/src/main/java/org/upgrad/controllers/AdminController.java
+++ Exercise-master/src/main/java/org/upgrad/controllers/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.upgrad.models.User;
 import org.upgrad.services.CategoryService;
 import org.upgrad.services.UserService;
 
@@ -21,13 +22,13 @@ public class AdminController {
      */
     @DeleteMapping("/api/admin/user/{userId}")
     public ResponseEntity<?> deleteUser(@PathVariable("userId") int userId, HttpSession session) {
-
-        if (session.getAttribute("currUser")==null) {
+        User user = (User) session.getAttribute("currUser");
+        if (user == null) {
             return new ResponseEntity<>("Please Login first to access this endpoint!", HttpStatus.UNAUTHORIZED);
         }
-        else if (session.getAttribute("currUserRole").equals("admin")) {
+        else if (user.getRole().equals("admin")) {
             userService.deleteUserById(userId);
-            return new ResponseEntity<>("User with userId: " + userId + " deleted successfully!", HttpStatus.OK);
+            return new ResponseEntity<>("User with userId " + userId + " deleted successfully!", HttpStatus.OK);
         }
         else {
             return new ResponseEntity<>("You do not have rights to delete a user!!", HttpStatus.UNAUTHORIZED);
@@ -38,9 +39,10 @@ public class AdminController {
      */
     @GetMapping("/api/admin/users/all")
     public ResponseEntity<?> getAllUsers(HttpSession session) {
-        if (session.getAttribute("currUser") == null) {
+        User user = (User) session.getAttribute("currUser");
+        if (user == null) {
             return new ResponseEntity<>("Please Login first to access this endpoint!", HttpStatus.UNAUTHORIZED);
-        } else if (session.getAttribute("currUserRole").equals("admin")) {
+        } else if (user.getRole().equals("admin")) {
             return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
         } else {
             return new ResponseEntity<>("You do not have rights to access all users!", HttpStatus.UNAUTHORIZED);
@@ -50,10 +52,11 @@ public class AdminController {
       accessible only by admin
      */
     @PostMapping("/api/admin/category")
-    public ResponseEntity<?> categoriesCreation (@RequestParam String title,@RequestParam String description, HttpSession session) {
-        if (session.getAttribute("currUser") == null) {
+    public ResponseEntity<?> categoriesCreation (@RequestParam("categoryTitle") String title,@RequestParam String description, HttpSession session) {
+        User user = (User) session.getAttribute("currUser");
+        if (user == null) {
             return new ResponseEntity<>("Please Login first to access this endpoint!", HttpStatus.UNAUTHORIZED);
-        } else if (session.getAttribute("currUserRole").equals("admin")) {
+        } else if (user.getRole().equals("admin")) {
             categoryService.updateCategory(title,description);
             return new ResponseEntity<>(title+" category added successfully.", HttpStatus.OK);
         } else {
